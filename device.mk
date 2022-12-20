3bca157038de7e7ee4ef150aecc43945bc715ab8
TheStrechh <TheStrechh@gmail.com>
2022-12-20 16:16:06 +0000

    spes: parts: Import XiaomiParts from surya

--- device.mk
+++ device.mk
@@ -165,6 +165,10 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Dex
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
 
